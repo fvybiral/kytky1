62ce28835ef6a56886f8eab5d272ab4710bce194
František Vybíral <frantisek.vybiral@avito.cz>
2024-04-12 20:24:15 +0200

    dark mode off

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -8,7 +8,7 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.css">
         <script src="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.js"></script>
     </head>
-    <body class="font-sans antialiased dark:bg-black dark:text-white/50 h-full">
+    <body class="font-sans antialiased h-full">
         @yield('page')
     </body>
 </html>
